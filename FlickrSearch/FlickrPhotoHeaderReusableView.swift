c69ceb66aa2eaffdd8e25188757d964c8ca32666
Dmitriy Roytman <d@geans.ru>
2017-07-27 11:02:28 +0400

    add header view

---
+++ FlickrSearch/FlickrPhotoHeaderReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoHeaderReusableView.swift
+//  FlickrSearch
+//
+//  Created by Dmitriy Roytman on 27.07.17.
+//  Copyright © 2017 Dmitriy Roytman. All rights reserved.
+//
+
+import UIKit
+
+final class FlickrPhotoHeaderReusableView: UICollectionReusableView {
+    @IBOutlet weak var label: UILabel!
+    
+}
